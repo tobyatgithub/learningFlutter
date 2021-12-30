7be1127a1c17854a6d5a1c68b431741a4c3a3d90
tobyatgithub <toby.fangyuan@gmail.com>
2021-12-29 16:59:51 -0800

    cache for firebasetutorial2.

---
+++ flutterWithFirebase2/flutter_firebase_app/lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
